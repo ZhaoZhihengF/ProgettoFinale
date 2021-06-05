cfd96ff0950214876799545062d4c1dc611aecdb
ZhaoZhihengF <zhao.zhiheng@iisgalvanimi.edu.it>
2021-06-05 16:04:55 +0000

    1

---
+++ PROGETTO/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express(); 
+const path = require('path');
+
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname,'Scarpe.html')); 
+});
+
+app.get('/AbbigliamentoMaschile',function(req,res){
+ res.sendFile(path.join(__dirname,'AbbigliamentoMaschile.html')); 
+});
+
+app.get('/AbbigliamentoFemminile',function(req,res){
+ res.sendFile(path.join(__dirname,'AbbigliamentoFemminile.html'));
+});
+app.get('/AbbigliamentoBambini',function(req,res){
+ res.sendFile(path.join(__dirname,'AbbigliamentoBambini.html'));
+});
+app.get('/Accesso',function(req,res){
+ res.sendFile(path.join(__dirname,'Accesso.html')); 
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
